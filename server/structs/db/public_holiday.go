5e9f0d7990896dea2260e2f24c4a0e4ab51ea8d1
Ivan Rusli <ivan.ru@tnis.com>
2018-12-20 19:43:06 +0700

    add id as primary key to table public_holiday

--- server/structs/db/public_holiday.go
+++ server/structs/db/public_holiday.go
@@ -2,6 +2,7 @@ package db
 
 // PublicHoliday ...
 type PublicHoliday struct {
+	ID          int64  `json:"id" orm:"column(id);pk"`
 	DateStart   int64  `json:"date_start" orm:"column(date_start)"`
 	DateEnd     int64  `json:"date_end" orm:"column(date_end)"`
 	Description string `json:"description" orm:"column(description)"`
