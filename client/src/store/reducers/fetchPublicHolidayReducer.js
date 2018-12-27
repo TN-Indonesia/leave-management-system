f92a0fd2700547ba0c290f019845cf3f99011d1e
Ivan Rusli <ivan.ru@tnis.com>
2018-12-27 10:25:52 +0700

    change route api name add Action and Reducer for public holiday

---
+++ client/src/store/reducers/fetchPublicHolidayReducer.js
@@ -0,0 +1,18 @@
+import {
+    FETCH_PUBLIC_HOLIDAY
+} from "../Actions/types"
+
+let publicHolidayState = {
+    publicHoliday: [],
+}
+
+export default function fetchTypeLeaveReducer(state = publicHolidayState, action) {
+    switch (action.type) {
+        case FETCH_PUBLIC_HOLIDAY:
+            return {
+                ...action.payload
+            }
+        default:
+            return state
+    }
+}
